1cc26ff991e55a79020e81fbccebf9232070dc2a
Niels Dyrberg <ncpd@live.dk>
2021-11-17 23:04:58 +0100

    Correction of name

--- src/debug_methods.cpp
+++ src/debug_methods.cpp
@@ -20,7 +20,7 @@ debug_write_file::~debug_write_file() {
 }
 
 void debug_write_file::write(const uint8_t* buff, int size) {
-    debug_fd.open("/tmp/rcv_debug_file.wav", std::ios::binary | std::ios::app);
+    debug_fd.open("/tmp/rcv_file.wav", std::ios::binary | std::ios::app);
     debug_fd.write((char*)buff, size);
     debug_fd.close();
     packets_received++;
